16a1cc44080f0173710c2e2d76e2cd56ca045ab9
GoodnessJames <akomagoodness97@gmail.com>
2024-02-06 00:44:30 +0100

    ES6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let __array = []
-    for (let value of array) {
-      __array.push(appendString + value);
-    }
+	let __array = [];
+	for (let value of array) {
+		__array.push(appendString + value);
+	}
   
-    return __array;
+	return __array;
 }
