d0cf53d1f49701b0325b96198d39321b1498826f
GoodnessJames <akomagoodness97@gmail.com>
2024-02-07 23:48:25 +0100

    Remove files

--- initial/0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-	let __array = [];
-	for (let value of array) {
-		__array.push(appendString + value);
-	}
-  
-	return __array;
-}
