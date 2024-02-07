2d38a0b2c0d7910ca9faf0646f39ddfb2839bbb0
GoodnessJames <akomagoodness97@gmail.com>
2024-02-07 23:23:14 +0100

    Initial failed codebase

---
+++ initial/0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+	let __array = [];
+	for (let value of array) {
+		__array.push(appendString + value);
+	}
+  
+	return __array;
+}
